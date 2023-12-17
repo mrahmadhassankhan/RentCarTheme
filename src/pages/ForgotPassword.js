c57e9acfb336ac2b8b9e74dee3f6da17b00db086
Ahmad Hassan Khan <fiverrahmadhassankhan@gmail.com>
2023-12-17 15:57:33 +0500

    Register Login and Forget Password Css Modified

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,27 +1,31 @@
 import React from "react";
+import styles from "../pages/LoginReg.module.css";
 import { Link } from "react-router-dom";
 export const ForgotPassword = () => {
   return (
-    <div>
-      {" "}
-      <section class="container forms">
-        <div class="form signup">
-          <div class="form-content">
+    <>
+      <section className={styles["container"]}>
+        <div className={styles["form "]}>
+          <div className={styles["form-content"]}>
             <header>Forgot Password</header>
             <form action="#">
-              <div class="field input-field">
-                <input type="email" placeholder="Email" class="input" />
+              <div className={styles["field input-field"]}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className={styles["input"]}
+                />
               </div>
 
-              <div class="field button-field">
+              <div className={styles["field button-field"]}>
                 <button>Submit</button>
               </div>
             </form>
 
-            <div class="form-link">
+            <div className={styles["form-link"]}>
               <span>
                 Back to Login-{" "}
-                <Link to="/login" class="link login-link">
+                <Link to="/login" className={styles["link login-link"]}>
                   Login
                 </Link>
               </span>
@@ -29,6 +33,6 @@ export const ForgotPassword = () => {
           </div>
         </div>
       </section>
-    </div>
+    </>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,72 +1,52 @@
 import React from "react";
-import "./LoginReg.css";
-import { Link } from "react-router-dom";
 
+import { Link } from "react-router-dom";
+import styles from "../pages/LoginReg.module.css";
 export default function Login() {
   return (
-    <>
-      <center>
-        <section className="container forms">
-          <div className="form login">
-            <div className="form-content">
-              <header>Login</header>
-              <form action="#">
-                <div className="field input-field">
-                  <input type="email" placeholder="Email" className="input" />
-                </div>
-
-                <div className="field input-field">
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    className="password"
-                  />
-                  <i className="bx bx-hide eye-icon"></i>
-                </div>
-
-                <div className="form-link">
-                  <Link to="/forgot-password" className="forgot-pass">
-                    Forgot password?
-                  </Link>
-                </div>
-
-                <div className="field button-field">
-                  <button>Login</button>
-                </div>
-              </form>
-
-              <div className="form-link">
-                <span>
-                  Don't have an account?{" "}
-                  <Link to="/register" className="link signup-link">
-                    Signup
-                  </Link>
-                </span>
-              </div>
+    <div className={styles["container"]}>
+      <div className={styles["forms"]}>
+        <div className={styles["form-content"]}>
+          <header>Login</header>
+          <form action="#">
+            <div className={styles["field input-field"]}>
+              <input
+                type="email"
+                placeholder="Email"
+                className={styles["input"]}
+              />
             </div>
 
-            <div className="line"></div>
-
-            <div className="media-options">
-              <a href="#" className="field facebook">
-                <i className="bx bxl-facebook facebook-icon"></i>
-                <span>Login with Facebook</span>
-              </a>
+            <div className={styles["field input-field"]}>
+              <input
+                type="password"
+                placeholder="Password"
+                className={styles["password"]}
+              />
+              <i className={styles["bx bx-hide eye-icon"]}></i>
             </div>
 
-            <div className="media-options">
-              <a href="#" className="field google">
-                <img
-                  src="/assets/images/google.png"
-                  alt=""
-                  className="google-img"
-                />
-                <span>Login with Google</span>
-              </a>
+            <div className={styles["form-link"]}>
+              <Link to="/forgot-password" className={styles["forgot-pass"]}>
+                Forgot password?
+              </Link>
             </div>
+
+            <div className={styles["field button-field"]}>
+              <button>Login</button>
+            </div>
+          </form>
+
+          <div className={styles["form-link"]}>
+            <span>
+              Don't have an account?{" "}
+              <Link to="/register" className={styles["link signup-link"]}>
+                Signup
+              </Link>
+            </span>
           </div>
-        </section>
-      </center>
-    </>
+        </div>
+      </div>
+    </div>
   );
 }
