f78867a3798b680f584a47afd57616afca632ed4
Ahmad Hassan Khan <fiverrahmadhassankhan@gmail.com>
2024-02-01 01:16:16 +0500

    added gitpages path

--- src/Components/MenuRoutes.js
+++ src/Components/MenuRoutes.js
@@ -22,6 +22,7 @@ export const MenuRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/RentCarTheme" element={<Home />}></Route>
         <Route path="/about-us" element={<Aboutus />}></Route>
         <Route path="/fleet" element={<Fleet />}></Route>
         <Route path="/services" element={<Services />}></Route>
